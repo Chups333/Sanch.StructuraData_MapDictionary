7c14f903793a37df3de90f590c8e8d730f679609
Petrov Sasha <sasha1327@mail.ru>
2020-08-27 19:32:12 +0800

    Добавьте файлы проекта.

---
+++ Sanch.StructuraData_MapDictionary/Model/Item.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sanch.StructuraData_MapDictionary.Model
+{
+    public class Item<TKey,TValue>
+    {
+        public TKey Key { get; set; }
+        public TValue Value { get; set; }
+        public Item(TKey key, TValue value)
+        {
+            //проверка
+            Key = key;
+            Value = value;
+        }
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+        public override int GetHashCode()
+        {
+            return Key.GetHashCode();
+        }
+    }
+}
